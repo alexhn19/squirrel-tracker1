39d64ac39609f6c13437b5a27bfd2f0b89a76a40
Alexander Han <axh2001@columbia.edu>
2020-10-14 03:57:13 +0000

    typo

--- project/sightings/urls.py
+++ project/sightings/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
         path('', views.index),
         path('sightings/add/',views.add_squirrel),
         path('sightings/stats/',views.get_stats),
-        path('sightings/<str:unique-squirrel-ID>/',views.edit_squirrel),
+        path('sightings/<str:unique_squirrel_ID>/',views.edit_squirrel),
         path('sightings/',views.sighting),
         ]
