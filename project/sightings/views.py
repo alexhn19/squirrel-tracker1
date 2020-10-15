3fbc65787c14c81142148f5b3b02f524f1ac3935
Alexander Han <axh2001@columbia.edu>
2020-10-15 02:48:46 +0000

    created stats function

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -7,4 +7,36 @@ def index(request):
 
 def map(request):
     return render(request, 'sightings/map.html', {})
-    
+
+def get_stats(request):
+    Chasing_count = 0
+    Eating_count = 0
+    Running_count = 0
+    Foraging_count = 0
+    Climbing_count = 0
+    length = 0
+    for i in Squirrel.objects.all():
+        if i.Chasing == True:
+            Chasing_count += 1
+        if i.Eating == True:
+            Eating_count += 1
+        if i.Climbing== True:
+            Climbing_count += 1
+        if i.Running == True:
+            Running_count += 1
+        if i.Foraging == True:
+            Foraging_count += 1
+        length += 1
+    running_percent=Running_count/length
+    chasing_percent=Chasing_count/length
+    eating_percent=Eating_count/length
+    foraging_percent=Foraging_count/length
+    climbing_percent=Climbing_count/length
+    context = {
+            'running_percent':running_percent,
+            'eating_percent':eating_percent,
+            'foraging_percent':foraging_percent,
+            'climbing_percent':climbing_percent,
+            'chasing_percent':chasing_percent,
+            }
+    return render(request, 'tracker_app/stats.html', context)
