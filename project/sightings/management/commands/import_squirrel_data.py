f77073d5d54f591d14c9627cbca1f9db85620b1e
peter-k-leung <63880619+peter-k-leung@users.noreply.github.com>
2020-10-15 21:59:27 -0400

    Update import_squirrel_data.py
    checking import error

--- project/sightings/management/commands/import_squirrel_data.py
+++ project/sightings/management/commands/import_squirrel_data.py
@@ -12,41 +12,47 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_ = options['csv_file']
+        
+        try:
 
-        with open(file_) as fp:
-            reader = csv.DictReader(fp)
+            with open(file_) as fp:
+                reader = csv.DictReader(fp)
 
-            for item in reader:
-                obj = Squirrel()
 
-                obj.latitude = item['X']
-                obj.longitude = item['Y']
-                obj.unique_squirrel_id = item['Unique Squirrel ID']
-                obj.hectare = item['Hectare']
-                obj.shift = item['Shift']
-                obj.date = item['Date']
-                obj.age = item['Age']
-                obj.primary_fur_color = item['Primary Fur Color']
-                obj.location = item['Location']
-                obj.specific_location = item['Specific Location']
-                obj.running = item['Running']
-                obj.chasing = item['Chasing']
-                obj.climbing = item['Climbing']
-                obj.eating = item['Eating']
-                obj.foraging = item['Foraging']
-                obj.other_activities = item['Other Activities']
-                obj.kuks = item['Kuks']
-                obj.quaas = item['Quaas']
-                obj.moans = item['Moans']
-                obj.tail_flags = item['Tail flags']
-                obj.tail_twitches = item['Tail twitches']
-                obj.approaches = item['Approaches']
-                obj.indifferent = item['Indifferent']
-                obj.runs_from = item['Runs from']
+                for item in reader:
+                    obj = Squirrel()
 
-                obj.save()
+                    obj.latitude = item['X']
+                    obj.longitude = item['Y']
+                    obj.unique_squirrel_id = item['Unique Squirrel ID']
+                    obj.hectare = item['Hectare']
+                    obj.shift = item['Shift']
+                    obj.date = item['Date']
+                    obj.age = item['Age']
+                    obj.primary_fur_color = item['Primary Fur Color']
+                    obj.location = item['Location']
+                    obj.specific_location = item['Specific Location']
+                    obj.running = item['Running']
+                    obj.chasing = item['Chasing']
+                    obj.climbing = item['Climbing']
+                    obj.eating = item['Eating']
+                    obj.foraging = item['Foraging']
+                    obj.other_activities = item['Other Activities']
+                    obj.kuks = item['Kuks']
+                    obj.quaas = item['Quaas']
+                    obj.moans = item['Moans']
+                    obj.tail_flags = item['Tail flags']
+                    obj.tail_twitches = item['Tail twitches']
+                    obj.approaches = item['Approaches']
+                    obj.indifferent = item['Indifferent']
+                    obj.runs_from = item['Runs from']
 
-            msg = f'You are importing from {file_}'
+                    obj.save()
 
-            self.stdout.write(self.style.SUCCESS(msg))
+                msg = f'You are importing from {file_}'
+
+                self.stdout.write(self.style.SUCCESS(msg))
+        except:
+            csv.Error as e:
+                print(f'there is something wrong with {fp.line_num}')
 
