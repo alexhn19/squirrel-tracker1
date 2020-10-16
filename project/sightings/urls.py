cb401a8295564848d2f6f30efbedf7c07e5a4d69
peter-k-leung <63880619+peter-k-leung@users.noreply.github.com>
2020-10-15 23:09:43 -0400

    Update urls.py

--- project/sightings/urls.py
+++ project/sightings/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         path('', views.index),
         path('sightings/add/',views.add_squirrel),
         path('sightings/stats/',views.get_stats),
-        path('sightings/<str:unique_squirrel_ID>/',views.update_squirrel),
+        path('sightings/<str:Unique_Squirrel_ID>/',views.update_squirrel),
         path('sightings/',views.sighting),
         path('map/',views.map),
         ]
