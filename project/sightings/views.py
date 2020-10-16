5b2f0f8c97564c36e0c49bdc80ba43b8f266f127
alexhn19 <61437005+alexhn19@users.noreply.github.com>
2020-10-16 00:34:33 -0400

    Update views.py

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Squirrel
 from .forms import SquirrelForm
