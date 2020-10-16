0936c75091d7a1f5a3b668ea1c86a052cdf1152a
peter-k-leung <63880619+peter-k-leung@users.noreply.github.com>
2020-10-15 22:56:33 -0400

    Update import_squirrel_data.py

--- project/sightings/management/commands/import_squirrel_data.py
+++ project/sightings/management/commands/import_squirrel_data.py
@@ -49,33 +49,33 @@ class Command(BaseCommand):
                 reader = csv.reader(f, dialect='excel')
                 next(reader,None)
                 try:
-                        for row in reader:
-                                squirrel = Squirrel.objects.create(
-                                X=row[0],
-                                Y=row[1],
-                                unique_squirrel_ID=row[2],
-                                Shift=row[4],
-                                Date = str(row[5])[4:] + '-' + str(row[5])[:2] + '-' + str(row[5])[2:4],
-                                Age=row[7],
-                                Primary_Fur_Color=row[8],
-                                Location=row[12],
-                                Specific_Location=row[14],
-                                Running=(row[15]=='true'),
-                                Chasing=(row[16]=='true'),
-                                Climbing=(row[17]=='true'),
-                                Eating=(row[18]=='true'),
-                                Foraging=(row[19]=='true'),
-                                Other_activities=row[20],
-                                Kuks=(row[21]=='true'),
-                                Quaas=(row[22]=='true'),
-                                Moans=(row[23]=='true'),
-                                Tail_flags=(row[24]=='true'),
-                                Tail_twitches=(row[25]=='true'),
-                                Approaches=(row[26]=='true'),
-                                Indifferent=(row[27]=='true'),
-                                Runs_From=(row[28] == 'true'),
-                                )
-                        squirrel.save()
-                 except:
-                        pass
+                    for row in reader:
+                        squirrel = Squirrel.objects.create(
+                        X=row[0],
+                        Y=row[1],
+                        unique_squirrel_ID=row[2],
+                        Shift=row[4],
+                        Date = str(row[5])[4:] + '-' + str(row[5])[:2] + '-' + str(row[5])[2:4],
+                        Age=row[7],
+                        Primary_Fur_Color=row[8],
+                        Location=row[12],
+                        Specific_Location=row[14],
+                        Running=(row[15]=='true'),
+                        Chasing=(row[16]=='true'),
+                        Climbing=(row[17]=='true'),
+                        Eating=(row[18]=='true'),
+                        Foraging=(row[19]=='true'),
+                        Other_activities=row[20],
+                        Kuks=(row[21]=='true'),
+                        Quaas=(row[22]=='true'),
+                        Moans=(row[23]=='true'),
+                        Tail_flags=(row[24]=='true'),
+                        Tail_twitches=(row[25]=='true'),
+                        Approaches=(row[26]=='true'),
+                        Indifferent=(row[27]=='true'),
+                        Runs_From=(row[28] == 'true'),
+                        )
+                    squirrel.save()
+                except:
+                    pass
 
